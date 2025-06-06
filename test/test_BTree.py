0999c2960d341455a1390a8de785d94eda7a17a1
Michael Jung <96927527+lolMichaelJung1@users.noreply.github.com>
2025-06-05 20:01:35 -0700

    Update test_BTree.py

--- test/test_BTree.py
+++ test/test_BTree.py
@@ -3,3 +3,14 @@ Unit tests for the BTree.py module.
 """
 
 import pytest
+
+from BTree import Tree234, Node234
+
+@pytest.fixture
+def test_BTree():
+  tree = Tree234()
+  integers = [10, 20, 5, 6, 12, 30, 25]
+  for i in integers:
+    tree.insert(i)  
+  for i in integers:
+    assert tree.contains(i)
